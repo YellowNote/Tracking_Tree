e5355187e696c97a07fdaf44b30bb02f8a00aa54
YellowNote <meislaxing@gmail.com>
2018-11-27 13:39:20 -0700

    Added struct for tree

--- Tracking_Tree/Tree.h
+++ Tracking_Tree/Tree.h
@@ -1,1 +1,19 @@
 #pragma once
+
+
+#include<string>
+#include<vector>
+
+using namespace std;
+
+struct  Tree
+{
+	string ID, ParentID, Event, LHash = NULL, RHash = NULL;
+	vector<string> LHist, RHist;
+};
+
+//TODO CreateID
+//TODO CreateEvent
+//TODO CreateNode
+//TODO UpdateHash
+//TODO UpdateHistory
